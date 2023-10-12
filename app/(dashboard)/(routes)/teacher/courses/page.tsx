3e5c6cb387d8c4a20f8cdec4484774fda7e9fb1a
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2023-10-12 14:31:07 +0300

    feat:added table courses

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -2,27 +2,43 @@ import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
+import { Payment, columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { redirect } from "next/navigation";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+ 
 
 const CoursesPage = async () => {
   const { userId } = auth();
-  const courses = await db.course.findMany({
+  
+  if (!userId) {
+    return redirect('/')
+  }
+  const data = await db.course.findMany({
     where: {
-      userId: userId,
+      userId
+    },
+    orderBy: {
+      createdAt: 'desc'
     }
   });
+
   
   return (
-    <div>
-      <Link href="/teacher/create">
-        <Button>New course</Button>
-      </Link>
-      {courses.map((item) => (
-        <div>
-          <Link href={`courses/${item.id}`}>
-            {item.title}
-          </Link>
-        </div>
-      ))}
+    <div className="p-4">
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
